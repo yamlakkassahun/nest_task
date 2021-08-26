d122586c113bfaae559c277e85d3a0a487813d44
Yamlak Kassahun <yamlak.k@gmail.com>
2021-08-26 16:00:02 +0300

    implimented the task creation and creat task create dto

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,19 @@ export class TasksService {
 
   getAllTasks() {
     return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    //this will distract the dto to title and description
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
