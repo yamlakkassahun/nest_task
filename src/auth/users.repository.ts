cfc9923a821c6e6e116d7e1a3768148ef6eb4061
Yamlak Kassahun <yamlak.k@gmail.com>
2021-09-06 16:59:43 +0300

    just started the auth module

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,36 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    //hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({
+      username,
+      password: hashedPassword,
+    });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        //this will handel the conflict error
+        throw new ConflictException('Username is Taken');
+      } else {
+        //this will handle other errors
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
