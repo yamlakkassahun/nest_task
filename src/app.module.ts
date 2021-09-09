453c055b0f850c612aa21d5074b271a4cd4b6417
yamlakkassahun <yamlak.k@gmail.com>
2021-09-09 03:59:33 -0700

    add configration to nest project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
