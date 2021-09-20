0aa00a510e6648620fd8d3ab4d09ef616d3dea17
Yamlak Kassahun <yamlak.k@gmail.com>
2021-09-20 11:21:19 +0300

    implimented the unit testing for the tasks service

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,9 +1,66 @@
-/* eslint-disable prettier/prettier */
+//this is a package that nestjs ship with to help us when testing
+import { NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { TaskStatus } from './task-status.enum';
 import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const mockUser = {
+  username: 'Zion',
+  id: 'someId',
+  password: 'somePassword',
+  tasks: [],
+};
+
 describe('TasksService', () => {
-    let tasksService: TasksService;
-    let tasksRepository: TasksRepository;
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    //initialize a nestJs module with tasksService and tasksRepository
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TasksService);
+    tasksRepository = module.get(TasksRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and return the result', async () => {
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(result).toEqual('someValue');
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls TasksRepository.findOne and return the result', async () => {
+      const mockTask = {
+        title: 'Test Title',
+        description: 'Test Description',
+        id: 'someId',
+        status: TaskStatus.OPEN,
+      };
+
+      tasksRepository.findOne.mockResolvedValue(mockTask);
+      const result = await tasksService.getTaskById('someId', mockUser);
+      expect(result).toEqual(mockTask);
+    });
+
+    it('calls TasksRepository.findOne and return the result', async () => {
+      tasksRepository.findOne.mockResolvedValue(null);
+      expect(tasksService.getTaskById('someId', mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
 });
